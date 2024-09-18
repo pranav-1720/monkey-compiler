515645838885e4922daa1013fecbd478a4d9abf1
Pranav Agrawal <pranavagrawal144@gmail.com>
2024-09-18 17:28:28 +0530

    Added support for arrays

--- code/code.go
+++ code/code.go
@@ -30,6 +30,7 @@ const (
 	OpNull
 	OpGetGlobal
 	OpSetGlobal
+	OpArray
 )
 
 var definitions = map[Opcode]*Definition{
@@ -51,6 +52,7 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
 }
 
 func Make(op Opcode, operands ...int) []byte {
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -187,6 +187,16 @@ func (c *Compiler) Compile(node ast.Node) error {
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
 
+	case *ast.ArrayLiteral:
+		for _, el := range node.Elements {
+			err := c.Compile(el)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpArray, len(node.Elements))
+
 	case *ast.Identifier:
 		symbol, ok := c.symbolTable.Resolve(node.Value)
 		if !ok {
